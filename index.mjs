6e1a12bc1dc4c103ff6a457486bb0ebf97a31213
victoriaeloho <ejumeguvictoria@gmail.com>
2022-07-19 17:12:48 +0100

    here too

---
+++ index.mjs
@@ -0,0 +1,32 @@
+import { loadStdlib } from '@reach-sh/stdlib';
+import * as backend from './build/index.main.mjs';
+
+const stdlib = loadStdlib(accCreator);
+//const  = stdlib.Launchtoken(accCreator);
+const assertEq = (expected, actual) => {
+  const exps = JSON.stringify(expected);
+  const acts = JSON.stringify(actual);
+  console.log('assertEq', {expected, actual}, {exps, acts});
+  stdlib.assert(exps === acts) };
+const startingBalance = stdlib.parseCurrency(100);
+const [ accAlice, accBob ] = await Promise.all([
+  stdlib.newTestAccount(startingBalance),
+  stdlib.newTestAccount(startingBalance),
+]);
+accAlice.setDebugLabel('Alice');
+accBob.setDebugLabel('Bob');
+const ctcAlice = accAlice.contract(backend);
+const ctcBob = accBob.contract(backend, ctcAlice.getInfo());
+const jayson = await stdlib.launchToken(accCreator, "jayson", "JYSN");
+//const gil = await stdlib.launchToken(accCreator, "gil", "GIL");
+
+const checkView = async (x, who, fe, ge) => {
+  console.log('checkView', x, who, stdlib.formatAddress(who), fe, ge);
+  assertEq(fe, await ctcAlice.v.Main.f(who));
+  assertEq(ge, await ctcAlice.v.Main.g(who));
+};
+
+await Promise.all([
+  backend.Alice(ctcAlice, { checkView }),
+  backend.Bob(ctcBob, { checkView }),
+]);
